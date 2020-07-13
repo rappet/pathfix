9a1629fcd2e4d41052838106d9f9e69929ce5aca
Raphael Peters <rappet@rappet.de>
2020-07-13 23:56:50 +0200

    Started config restructoration

---
+++ src/config.rs
@@ -0,0 +1,105 @@
+use serde::{Serialize, Deserialize};
+
+use std::collections::HashMap;
+
+#[derive(Default, Debug, Clone, Serialize, Deserialize, Eq, PartialEq)]
+pub struct Config {
+    // Do not read in higher directories
+    pub base: bool,
+    pub include_administrative: Option<IncludeAdministrative>,
+    #[serde(default)]
+    pub paths: Vec<SyntaxSuggarPath>,
+    #[serde(default)]
+    pub default_env: HashMap<String, String>,
+}
+
+impl Config {
+    pub fn included() -> Config {
+        serde_yaml::from_str(include_str!("config.yml")).unwrap()
+    }
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize, Eq, PartialEq)]
+#[serde(rename_all = "kebab-case")]
+pub enum IncludeAdministrative {
+    Always,
+    RootOnly,
+    Groups(Vec<String>),
+    Never
+}
+
+impl Default for IncludeAdministrative {
+    fn default() -> Self {
+        IncludeAdministrative::Groups(vec![
+            "wheel".to_string(),
+            "sudo".to_string(),
+        ])
+    }
+}
+
+#[derive(Default, Debug, Clone, Serialize, Deserialize, Eq, PartialEq)]
+pub struct Path {
+    pub path: String,
+    pub admin: bool,
+}
+
+impl Path {
+    pub fn resolve(&self, env: &HashMap<String, String>) -> Option<String> {
+        let path: Option<Vec<String>> = self.path
+            .split('/')
+            .map(|folder| {
+                if folder == "~" {
+                    env.get("HOME").map(String::to_string)
+                } else if folder.chars().next() == Some('$') {
+                    env.get(&folder[1..]).map(String::to_string)
+                } else {
+                    Some(folder.to_string())
+                }
+            })
+            .collect();
+        path.map(|path| path.join("/"))
+    }
+}
+
+impl<S> From<S> for Path
+where S: Into<String> {
+    fn from(s: S) -> Self {
+        Path {
+            path: s.into(),
+            ..Default::default()
+        }
+    }
+}
+
+impl From<SyntaxSuggarPath> for Path {
+    fn from(path: SyntaxSuggarPath) -> Self {
+        match path {
+            SyntaxSuggarPath::Simple(s) =>  s.into(),
+            SyntaxSuggarPath::Struct(s) => s
+        }
+    }
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize, Eq, PartialEq)]
+#[serde(untagged)]
+pub enum SyntaxSuggarPath {
+    Simple(String),
+    Struct(Path)
+}
+
+impl<S> From<S> for SyntaxSuggarPath
+where S: ToString {
+    fn from(path: S) -> Self {
+        SyntaxSuggarPath::Struct(path.to_string().into())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::config::Config;
+
+    #[test]
+    fn test_included() {
+        Config::included();
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,53 +1,40 @@
 #[macro_use]
 extern crate clap;
+extern crate serde;
+extern crate serde_yaml;
 
 use clap::App;
 
 use std::collections::HashSet;
 
-mod path;
 mod cli;
+mod config;
 
-use path::ConfigurablePath;
 use cli::{CliConfig, Mode};
+use config::Config;
 use std::borrow::Borrow;
+use crate::config::Path;
 
-fn default_paths() -> Vec<ConfigurablePath<'static>> {
-    vec![
-        ConfigurablePath::with_env(".cargo/bin", "HOME", "/"),
-        ConfigurablePath::with_env("bin", "GOPATH", "/usr/local/go"),
-        ConfigurablePath::with_env("bin", "GOROOT", "/usr/local/go"),
-        ConfigurablePath::with_env(".local/bin", "HOME", "/"),
-        ConfigurablePath::with_env("bin", "HOME", "/"),
-        ConfigurablePath::new("/snap"),
-        ConfigurablePath::new_become("/usr/local/sbin"),
-        ConfigurablePath::new("/usr/local/bin"),
-        ConfigurablePath::new_become("/usr/sbin"),
-        ConfigurablePath::new("/usr/bin"),
-        ConfigurablePath::new_become("/sbin"),
-        ConfigurablePath::new("/bin"),
-        ConfigurablePath::new("/usr/local/games"),
-        ConfigurablePath::new("/usr/games"),
-    ]
-}
-
-fn run(config: &CliConfig) -> Result<(), &'static str> {
+fn run(cli: &CliConfig) -> Result<(), &'static str> {
     let path_str = std::env::var("PATH").or(Err("Could not geth $PATH environment variable"))?;
-    let mut path: Vec<String> = if config.from_env {
+    let mut path: Vec<String> = if cli.from_env {
         path_str.split(':').map(String::from).collect()
     } else {
         Vec::new()
     };
 
-    if config.with_searches {
-        let mut searches: Vec<String> = default_paths().iter()
-            .filter_map(ConfigurablePath::to_std_pathbuf_checked)
-            .map(|p| String::from(p.to_str().expect("Path has non utf8 characters")))
-            .collect();
-        path.append(&mut searches);
-    }
+    // TODO scan for config
 
-    if config.dedup {
+    let config = Config::included();
+
+    // TODO add config from env
+
+    path = config.paths.iter()
+        .map(|p| Path::from(p.clone()))
+        .filter_map(|p| p.resolve(&config.default_env))
+        .collect();
+
+    if cli.dedup {
         // removes duplicates while preserving order
         let (new_path, _set) = path.iter().fold(
             (Vec::new(), HashSet::new()),
@@ -62,7 +49,7 @@ fn run(config: &CliConfig) -> Result<(), &'static str> {
     }
     
     // print output to stdout
-    match config.mode {
+    match cli.mode {
         Mode::Colon => {
             println!("{}", path.join(":"));
         },
