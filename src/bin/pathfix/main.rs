757489ab62559b7883f8328ade5ff4e3ef88b5e7
Raphael Peters <rappet@rappet.de>
2020-07-20 21:20:46 +0200

    bin: Added checking for admin and fixing some config merging bugs

--- src/bin/pathfix/main.rs
+++ src/bin/pathfix/main.rs
@@ -15,15 +15,16 @@ use std::collections::HashSet;
 use std::io;
 
 use std::borrow::Borrow;
-use pathfix::config::Config;
+use pathfix::config::{Config, IncludeAdministrative};
 use pathfix::config::{Path, Paths};
 
-fn run(cli: &CliConfig) -> Result<(), String> {
+fn run(cli: &CliConfig) -> Result<(), io::Error> {
     let mut env_config = Config::new().with_env();
 
     // Use paths from environment if -e is set
     if cli.from_env {
-        env_config.paths = Paths::from_env().map_err(|_| "$PATH does not contain valid utf8")?;
+        env_config.paths = Paths::from_env()
+            .map_err(|_| io::Error::new(io::ErrorKind::InvalidData, "$PATH does not contain valid utf8"))?;
         info!("Loaded contents of $PATH variable");
     }
 
@@ -37,13 +38,13 @@ fn run(cli: &CliConfig) -> Result<(), String> {
                     io::ErrorKind::NotFound => Ok(None),
                     _ => Err(err)
                 })
-                .map_err(|err| err.to_string())
         };
 
         if let Some(home_config) = load_config(
-            &format!("{}/.pathfix.toml", std::env::var("HOME").map_err(|err| err.to_string())?)
+            &format!("{}/.pathfix.toml", std::env::var("HOME")
+                .map_err(|_err| io::Error::new(io::ErrorKind::InvalidData, "could not load $HOME var"))?)
         )? {
-            config = config.merge(home_config);
+            config = home_config.merge(config);
             info!("Loaded config from ~/.pathfix.toml");
         } else {
             info!("~/.pathfix.toml is missing")
@@ -51,7 +52,7 @@ fn run(cli: &CliConfig) -> Result<(), String> {
 
         if !config.base {
             if let Some(etc_config) = load_config("/etc/pathfix.toml")? {
-                config = config.merge(etc_config);
+                config = etc_config.merge(config);
                 info!("Loaded config from /etc/pathfix.toml");
             } else {
                 info!("/etc/pathfix.toml is missing")
@@ -63,13 +64,18 @@ fn run(cli: &CliConfig) -> Result<(), String> {
             let included_config = Config::included();
 
             // Merge included config and config from path
-            config = config.merge(included_config);
+            config = included_config.merge(config);
             info!("Loaded included config")
         };
     }
 
+    let include_admin = config.include_administrative.as_ref()
+        .unwrap_or(&IncludeAdministrative::RootOnly)
+        .check_current_user()?;
+
     let mut path: Vec<String> = config.paths.iter()
         .map(|p| Path::from(p.clone()))
+        .filter(|p| !p.admin || include_admin)
         .filter_map(|p| p.resolve(&config.env))
         .collect();
 
@@ -86,6 +92,9 @@ fn run(cli: &CliConfig) -> Result<(), String> {
             });
         path = new_path;
     }
+
+    eprintln!("{:?}", config.include_administrative.clone().unwrap_or(IncludeAdministrative::default()));
+    eprintln!("{:?}", config.include_administrative.clone().unwrap_or(IncludeAdministrative::default()).check_current_user());
 
     // print output to stdout
     match cli.mode {
